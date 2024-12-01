d8e05b952c612fa64164f4a5fe179a378751679c
brhmpreet <4366brahampreet@gmail.com>
2024-12-01 15:29:06 +0530

    Product filter

---
+++ script.js
@@ -0,0 +1,39 @@
+const productList = document.getElementById("product-list");
+const serachInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+const categoryBtns = document.querySelectorAll(".category-btn");
+
+function filterProduct() {
+  const searchValue = serachInput.value.toLowerCase();
+  const productItems = document.querySelectorAll(".product-item");
+  const activeCategory = document.querySelector(".category-btn.active").dataset
+    .category;
+  productItems.forEach((item) => {
+    const title = item.querySelector("h3").innerText.toLowerCase();
+    const category = item.dataset.category;
+    if (
+      (title.includes(searchValue) || searchValue == "") &&
+      (category === activeCategory || activeCategory === "all")
+    ) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+function setCategory(e) {
+  categoryBtns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  e.target.classList.add("active");
+  filterProduct();
+}
+
+searchBtn.addEventListener("click", filterProduct);
+serachInput.addEventListener("keyup", filterProduct);
+categoryBtns.forEach((btn) => {
+  btn.addEventListener("click", setCategory);
+});
+
+filterProduct();
